9fd3d0506798b3a9b70b0dc8c360c99eaa72ff23
CatarinaSilva <catarina.cruz.csilva@gmail.com>
2017-09-04 22:02:13 +0100

    Fix version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='unbabel_subword_nmt',
-      version='0.2dev',
+      version='v0.1.1',
       description='Subword through Byte Pair Encoding',
       author='Unbabel',
       author_email='support@unbabel.com',
